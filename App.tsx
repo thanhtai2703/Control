6613796d1b2b30102d7d59c2e80cfcc48b7b4fa6
Truong Thanh Tai <thanhtai05.work@gmail.com>
2025-03-10 19:49:41 +0700

    to do list

--- App.tsx
+++ App.tsx
@@ -1,45 +1,56 @@
 
 import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
-import { Button, FlatList, InputAccessoryView, StyleSheet, Text, TextInput, View } from 'react-native';
+import { Alert, Button, FlatList, InputAccessoryView, Pressable, StyleSheet, Text, TextInput, Touchable, TouchableOpacity, View } from 'react-native';
 
-interface ITtodo{
-  id:number;
+interface ITtodo {
+  id: number;
   name: string
 }
 export default function App() {
-  const [todo,setTodo] = useState("")
-  const [listTodo,SetlistTodo] = useState<ITtodo[]>([])
-  function RandomInteger(min:number,max:number){
-    return Math.floor(Math.random()*(max - min + 1) + min)
+  const [todo, setTodo] = useState("")
+  const [listTodo, SetlistTodo] = useState<ITtodo[]>([])
+  function RandomInteger(min: number, max: number) {
+    return Math.floor(Math.random() * (max - min + 1) + min)
   }
   const handleAddTodo = () => {
-    if(!todo) return;
-    SetlistTodo([...listTodo,{id:RandomInteger(1,1000),name:todo}
+    if (!todo) {
+      return;
+    }
+    SetlistTodo([...listTodo, { id: RandomInteger(1, 1000), name: todo }
     ])
     setTodo('')
-  } 
+  }
+    const deleteTodo = (id:number )=>{
+      const newTodo = listTodo.filter(item=>item.id !== id );
+      SetlistTodo(newTodo)
+    }
   return (
     <View style={styles.container}>
-      <Text style ={styles.header}>Hello world</Text>
-    <View style ={{marginBottom : 10}}>
-      <TextInput style ={styles.todo}
-      value= {todo}
-      onChangeText={(value) => setTodo(value)}
-      />
-        <Button title='Add'
-        onPress={handleAddTodo}
-        
+      <Text style={styles.header}>To Do List</Text>
+      <View style={{ marginBottom: 10 }}>
+        <TextInput style={styles.todo}
+          value={todo}
+
+          onChangeText={(value) => setTodo(value)}
         />
-    </View>
-    <View>
-      <FlatList
-      data={listTodo}
-      renderItem={({item})=>{
-        return <Text style ={styles.todoItem}>{item.name}</Text>
-      }}
-      />
-    </View>
+        <Button title='Add'
+          onPress={handleAddTodo}
+        />
+      </View>
+      <View>
+        <FlatList
+          data={listTodo}
+          keyExtractor={item=> item.id+""}
+          renderItem={({ item }) => {
+            return (
+              <Pressable onPress={()=>deleteTodo(item.id)}>
+            <Text style={styles.todoItem}>{item.name}</Text>
+            </Pressable>
+            )
+          }}
+        />
+      </View>
     </View>
   );
 }
@@ -47,21 +58,21 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding : 20
+    padding: 20
   },
-  header:{
-    backgroundColor : "blue",
+  header: {
+    backgroundColor: "blue",
     textAlign: 'center',
-    fontSize: 20
-
+    marginHorizontal: 10,
+    fontSize: 40,
   },
-  todo :{
+  todo: {
     borderWidth: 1,
     borderColor: 'green',
-    marginTop : 10,
+    marginTop: 10,
     marginBottom: 15
   },
-  todoItem:{
+  todoItem: {
     fontSize: 20,
     marginBottom: 10,
     borderWidth: 1,
